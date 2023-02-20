64063072c0847d466597ddd33ccd84cc09c37a30
Dan Holodak <danholodak@gmail.com>
2023-02-20 15:18:43 -0500

    added some user model

--- practice-for-ch-rails-ninety-nine-cats1-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/app/models/user.rb
@@ -1,2 +1,30 @@
 class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence:true
+
+    validates :password, length: {minimum: 6}
+
+    attr_reader :password
+
+    def password=(pw)
+        @password = pw
+        self.password_digest = BCrypt::Password.create(pw)
+    end
+
+    def is_password?(pw)
+        bcrypt = BCrypt::Password.new(self.password_digest)
+        bcrypt.is_password?(pw)
+
+    end
+
+    def self.find_by_credentials(username, password)
+        @user = User.find(params[:user][:username], params[:user][:password])
+        if @user && @user.is_password?(password)
+            return @user
+        else
+            return nil
+        end
+    end
+
+
 end
